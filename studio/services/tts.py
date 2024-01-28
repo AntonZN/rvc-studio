915fd8f8b4630d3025ac5fa0a9c40a2650bb41e6
a.zinoviev <a.zinoviev@sb117.ru>
2024-01-28 17:58:53 +0700

    fx

--- studio/services/tts.py
+++ studio/services/tts.py
@@ -53,8 +53,10 @@ def tts(tts_id, model_name, text: str, lang: str, speaker=0):
     model = BarkModel.from_pretrained(f"{BASE_MODELS_DIR}/bark-small")
     processor = BarkProcessor.from_pretrained(f"{BASE_MODELS_DIR}/bark-small")
     inputs = processor(text, voice_preset=f"v2/{lang}_speaker_{speaker}")
+    attention_mask = inputs.ne(processor.pad_token_id)
     speech_output = (
-        model.generate(**inputs).cpu().numpy().squeeze() * MAX_INT16
+        model.generate(**inputs, attention_mask=attention_mask).cpu().numpy().squeeze()
+        * MAX_INT16
     ).astype(np.int16)
     sampling_rate = model.generation_config.sample_rate
     audio_data = audio2bytes(speech_output, sr=sampling_rate)
