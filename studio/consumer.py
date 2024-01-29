d7af5c1a060fb4924ddf5d00e0e29af984403aa9
a.zinoviev <a.zinoviev@sb117.ru>
2024-01-29 14:13:47 +0700

    fx

--- studio/consumer.py
+++ studio/consumer.py
@@ -173,6 +173,7 @@ async def consume(loop, queue_name: str, worker) -> None:
 
                 async with queue.iterator() as queue_iter:
                     async for message in queue_iter:
+                        loguru.logger.debug(f"Worker[{worker}][{queue_name}] New message")
                         loguru.logger.debug(f"Worker[{worker}][{queue_name}] New message {message})")
                         await handle(queue_name, message.body.decode())
                         loguru.logger.debug(f"Worker[{worker}][{queue_name}] Message ASK)")
