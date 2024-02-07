32c117b37c4365fb1e64b79ae978f87e89a590be
a.zinoviev <a.zinoviev@sb117.ru>
2024-02-07 19:03:01 +0700

    fx

--- admin/src/studio/models.py
+++ admin/src/studio/models.py
@@ -13,7 +13,7 @@ class Record(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField("Название", max_length=256)
     file_path = models.TextField()
-    status = models.CharField(choices=Status, default=Status.PENDING, max_length=100)
+    status = models.CharField(choices=Status.choices, default=Status.PENDING, max_length=100)
     vocal_path = models.TextField(null=True, blank=True)
     instrumental_path = models.TextField(null=True, blank=True)
     clone_path = models.TextField(null=True, blank=True)
