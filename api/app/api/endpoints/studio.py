5a8fb7e4c0d4bb35caccee48cbb12406ec040392
a.zinoviev <a.zinoviev@sb117.ru>
2024-03-26 09:27:16 +0700

    fx

--- api/app/api/endpoints/studio.py
+++ api/app/api/endpoints/studio.py
@@ -119,9 +119,13 @@ async def create_statistics(process):
         start_of_day = datetime.datetime.combine(date, datetime.time.min)
         end_of_day = datetime.datetime.combine(date, datetime.time.max)
 
-        times = await ProcessRequest.filter(
-            created_at__gte=start_of_day, created_at__lte=end_of_day
-        ).values_list("waiting_time_in_seconds", flat=True)
+        times = (
+            await ProcessRequest.filter(
+                created_at__gte=start_of_day, created_at__lte=end_of_day
+            )
+            .exclude(process_type="old_split")
+            .values_list("waiting_time_in_seconds", flat=True)
+        )
 
         time_waiting = list(times)
 
