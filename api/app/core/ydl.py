355aaf6344aa2fc33e92218e5c47aa4f75394fa2
a.zinoviev <a.zinoviev@sb117.ru>
2024-06-28 15:20:03 +0700

    fx

--- api/app/core/ydl.py
+++ api/app/core/ydl.py
@@ -15,27 +15,45 @@ from pytube.innertube import _cache_dir
 def download_youtube_video_as_mp3(url, output_path, max_duration=300, trim_duration=30):
     os.makedirs(output_path, exist_ok=True)
 
-    yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
-    video_duration = yt.length
+    try:
+        yt = YouTube(url, use_oauth=True, allow_oauth_cache=True)
+        video_duration = yt.length
+    except Exception as e:
+        loguru.logger.error(f"Ошибка получения видео: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="INCORRECT_LINK",
+        )
 
     if video_duration > max_duration:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail=(
-                f"Длительность видео ({video_duration} секунд) превышает "
-                f"максимальную длительность ({max_duration} секунд). Видео не будет загружено."
-            ),
+            detail="INCORRECT_VIDEO_DURATION"
         )
 
-    video = yt.streams.filter(only_audio=True).first()
-    downloaded_file = video.download(output_path=output_path)
+    try:
+        video = yt.streams.filter(only_audio=True).first()
+        downloaded_file = video.download(output_path=output_path)
+    except Exception as e:
+        loguru.logger.error(f"Ошибка загрузки видео: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"DOWNLOAD_ERROR"
+        )
+    try:
+        audio = AudioSegment.from_file(downloaded_file)
+        trimmed_audio = audio[: trim_duration * 1000]
+        file_name = f"{uuid4()}_trimmed.mp3"
+        trimmed_file = os.path.join(output_path, file_name)
+        trimmed_audio.export(trimmed_file, format="mp3")
+    except Exception as e:
+        loguru.logger.error(f"Ошибка обработки аудио: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"AUDIO_PROCESSING_ERROR"
+        )
+    finally:
+        if os.path.exists(downloaded_file):
+            os.remove(downloaded_file)
 
-    audio = AudioSegment.from_file(downloaded_file)
-
-    trimmed_audio = audio[: trim_duration * 1000]
-    file_name = f"{uuid4()}_trimmed.mp3"
-    trimmed_file = os.path.join(output_path, file_name)
-    trimmed_audio.export(trimmed_file, format="mp3")
-
-    os.remove(downloaded_file)
     return trimmed_file, file_name
