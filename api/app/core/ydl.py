e435a5c1f262b2f28da3c696d0e661903bc9a6ee
a.zinoviev <a.zinoviev@sb117.ru>
2024-07-21 19:52:49 +0700

    fx

--- api/app/core/ydl.py
+++ api/app/core/ydl.py
@@ -1,3 +1,57 @@
+from pytube import cipher
+import re
+
+from pytube.exceptions import RegexMatchError
+
+
+def get_throttling_function_name(js: str) -> str:
+    """Extract the name of the function that computes the throttling parameter.
+
+    :param str js:
+        The contents of the base.js asset file.
+    :rtype: str
+    :returns:
+        The name of the function used to compute the throttling parameter.
+    """
+    function_patterns = [
+        # https://github.com/ytdl-org/youtube-dl/issues/29326#issuecomment-865985377
+        # https://github.com/yt-dlp/yt-dlp/commit/48416bc4a8f1d5ff07d5977659cb8ece7640dcd8
+        # var Bpa = [iha];
+        # ...
+        # a.C && (b = a.get("n")) && (b = Bpa[0](b), a.set("n", b),
+        # Bpa.length || iha("")) }};
+        # In the above case, `iha` is the relevant function name
+        r'a\.[a-zA-Z]\s*&&\s*\([a-z]\s*=\s*a\.get\("n"\)\)\s*&&\s*'
+        r"\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])?\([a-z]\)",
+        r"\([a-z]\s*=\s*([a-zA-Z0-9$]+)(\[\d+\])\([a-z]\)",
+    ]
+    # logger.debug('Finding throttling function name')
+    for pattern in function_patterns:
+        regex = re.compile(pattern)
+        function_match = regex.search(js)
+        if function_match:
+            # logger.debug("finished regex search, matched: %s", pattern)
+            if len(function_match.groups()) == 1:
+                return function_match.group(1)
+            idx = function_match.group(2)
+            if idx:
+                idx = idx.strip("[]")
+                array = re.search(
+                    r"var {nfunc}\s*=\s*(\[.+?\]);".format(
+                        nfunc=re.escape(function_match.group(1))
+                    ),
+                    js,
+                )
+                if array:
+                    array = array.group(1).strip("[]").split(",")
+                    array = [x.strip() for x in array]
+                    return array[int(idx)]
+
+    raise RegexMatchError(caller="get_throttling_function_name", pattern="multiple")
+
+
+cipher.get_throttling_function_name = get_throttling_function_name
+
 import os
 import time
 import shutil
@@ -27,8 +81,7 @@ def download_youtube_video_as_mp3(url, output_path, max_duration=300, trim_durat
 
     if video_duration > max_duration:
         raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="INCORRECT_VIDEO_DURATION"
+            status_code=status.HTTP_400_BAD_REQUEST, detail="INCORRECT_VIDEO_DURATION"
         )
 
     try:
@@ -37,8 +90,7 @@ def download_youtube_video_as_mp3(url, output_path, max_duration=300, trim_durat
     except Exception as e:
         loguru.logger.error(f"Ошибка загрузки видео: {e}")
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"DOWNLOAD_ERROR"
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"DOWNLOAD_ERROR"
         )
     try:
         audio = AudioSegment.from_file(downloaded_file)
@@ -50,7 +102,7 @@ def download_youtube_video_as_mp3(url, output_path, max_duration=300, trim_durat
         loguru.logger.error(f"Ошибка обработки аудио: {e}")
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"AUDIO_PROCESSING_ERROR"
+            detail=f"AUDIO_PROCESSING_ERROR",
         )
     finally:
         if os.path.exists(downloaded_file):
